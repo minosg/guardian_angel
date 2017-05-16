428345b14701d337e5e39a1b844e14eaacd4b49b
Minos Galanakis <minos197@gmail.com>
2017-05-16 16:59:01 +0100

    autoinstall: Added copyssh and git checkout commands

--- autoinstall/autoinstall.sh
+++ autoinstall/autoinstall.sh
@@ -25,19 +25,25 @@ fi
 # Upload the files and install them
 if [ "$#" -eq 3 ] && [ "$1" = "--upload" ] && [ "$2" = "--install" ]; then
     statusmsg "Uploading Scripts to host $3 " "PREP"
-    RET=$(upload raspberrypi pi raspberry ./\*)
+    RET=$(upload raspberrypi pi raspberry "[!.]*")
     statusmsg "Uploading Scripts to host $3 " "$RET"
 
     if [ "$RET" != "DONE" ]; then exit 1; else RET="";fi;
 
-    statusmsg "Enable Root with password $ROOT_PWD" "PREP"
-    RET=$(enableroot $ROOT_PWD $3)
-    statusmsg "Enable Root with password $ROOT_PWD" "$RET"
+    statusmsg "Enable Root with password $ROOTPWD" "PREP"
+    RET=$(enableroot $ROOTPWD $3)
+    statusmsg "Enable Root with password $ROOTPWD" "$RET"
 
     if [ "$RET" != "DONE" ]; then exit 1; else RET="";fi;
 
+    if [ -s $SSHFILE ];then
+        statusmsg "Copy ssh keys from $SSHFILE to authorized_hosts" "PREP"
+        RET=$(copysshid root $ROOTPWD $3 $SSHFILE)
+        statusmsg "Copy ssh keys from $SSHFILE to authorized_hosts" "$RET"
+    fi
+
     statusmsg "Run remote autoinstall" "PREP"
-    RET=$(autoinstall $3 $ROOT_PWD)
+    RET=$(autoinstall $3 $ROOTPWD)
     statusmsg "Run remote autoinstall" "$RET"
     exit 0;
 fi
@@ -48,20 +54,30 @@ if [ "$#" -ge 2 ] && [ "$1" = "--login" ]; then
     exit 0;
 fi
 
+# Copy SSH keys from file to authorized hosts
+if [ "$#" -ge 2 ] && [ "$1" = "--copyid" ]; then
+
+    statusmsg "Copy ssh keys from $SSHFILE to authorized_hosts" "PREP"
+    RET=$(copysshid root $ROOTPWD $2 $SSHFILE)
+    statusmsg "Copy ssh keys from $SSHFILE to authorized_hosts" "$RET"
+    exit 0;
+fi
+
+
 # Enable root to local pi device
 if [ "$#" -eq 1 ] && [ "$1" = "--enableroot" ]; then
-    statusmsg "Enable Root with password $ROOT_PWD" "PREP"
-    RET=$(enableroot $ROOT_PWD)
-    statusmsg "Enable Root with password $ROOT_PWD" "$RET"
+    statusmsg "Enable Root with password $ROOTPWD" "PREP"
+    RET=$(enableroot $ROOTPWD)
+    statusmsg "Enable Root with password $ROOTPWD" "$RET"
     echo -e "Please reboot and log-in as root with password $ROOTPWD"
     exit 0;
 fi
 
 # Enable root to remote pi device
 if [ "$#" -eq 2 ] && [ "$1" = "--enableroot" ]; then
-    statusmsg "Enable Root with password $ROOT_PWD" "PREP"
-    RET=$(enableroot $ROOT_PWD $2)
-    statusmsg "Enable Root with password $ROOT_PWD" "$RET"
+    statusmsg "Enable Root with password $ROOTPWD" "PREP"
+    RET=$(enableroot $ROOTPWD $2)
+    statusmsg "Enable Root with password $ROOTPWD" "$RET"
     exit 0;
 fi
 
@@ -93,18 +109,33 @@ if [ "$#" -eq 1 ] && [ "$1" = "--install" ]; then
 
     # Install packages using apt-get install
     statusmsg "Installing debian packages from $APTFILE" "PREP"
+    echo -e ""
     multiinstall $APTFILE
-    statusmsg "Installing debian packages from $APTFILE" "DONE"
+    statusmsg "Installing debian packages from $APTFILE" "$(ret2print $?)"
 
     # Install local prepackaged files
     statusmsg "Installing local debian packages from $DEBDIR" "PREP"
+    echo -e ""
     debinstall $DEBDIR
-    statusmsg "Installing debian packages from $DEBDIR" "DONE"
+    statusmsg "Installing debian packages from $DEBDIR" "$(ret2print $?)"
 
     # Install packages using pip
     statusmsg "Installing pip packages from $PIPFILE" "PREP"
+    echo -e ""
     pipinstall $PIPFILE
-    statusmsg "Installing pip packages from $PIPFILE" "DONE"
+    statusmsg "Installing pip packages from $PIPFILE" "$(ret2print $?)"
 
+    # Checkout code in git
+    statusmsg "Checking out code from $GITFILE" "PREP"
+    echo -e ""
+    CDIR=$(pwd)
+    mkdir -p $GITDIR && cd $GITDIR
+    RET=$(gitcheckout $GITFILE)
+    cd $CDIR
+    statusmsg "Checking out code from $GITFILE" "$RET"
+
+    statusmsg "Autoinstall Completed" "DONE"
     exit 0;
 fi
+
+showhelp
